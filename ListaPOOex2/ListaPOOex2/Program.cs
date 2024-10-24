fddff843fea77f2e8e4d476f3456c772cc210ec6
GeovanySantos <165846992+GeovanySantos@users.noreply.github.com>
2024-10-24 19:55:56 -0300

    Add files via upload

---
+++ ListaPOOex2/ListaPOOex2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Quadrado
+{
+    public double Aresta { get; set; }
+
+    public double CalcularArea()
+    {
+        return Aresta * Aresta;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Quadrado quadrado = new Quadrado();
+        quadrado.Aresta = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine($"A área do quadrado é: {quadrado.CalcularArea()}");
+    }
+}
